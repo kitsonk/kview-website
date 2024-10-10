4d5d2b528b8baf08f4464b392bdd5e8024e694cb
Kitson Kelly <me@kitsonkelly.com>
2024-10-10 15:15:04 +1100

    move install redirect to JSR

--- routes/install.ts
+++ routes/install.ts
@@ -1,9 +1,23 @@
 import { type Handlers } from "$fresh/server.ts";
 
+let latest: string | undefined;
+
 export const handler: Handlers = {
-  GET(req) {
+  async GET(req) {
     if (req.headers.get("user-agent")?.startsWith("Deno")) {
-      return Response.redirect("https://deno.land/x/kview/install.ts", 307);
+      if (!latest) {
+        const res = await fetch("https://jsr.io/@kitsonk/kview/meta.json");
+        if (res.ok) {
+          const { latest: l } = await res.json();
+          latest = l;
+        } else {
+          return Response.error();
+        }
+      }
+      return Response.redirect(
+        `https://jsr.io/@kitsonk/kview/${latest}/install.ts`,
+        307,
+      );
     }
     return Response.redirect("/", 301);
   },
