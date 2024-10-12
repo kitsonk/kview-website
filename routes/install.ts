382b5802a2075b1ab8858066bd79107b0d17528b
Kitson Kelly <me@kitsonkelly.com>
2024-10-12 19:59:45 +1100

    repoint install at deno.land

--- routes/install.ts
+++ routes/install.ts
@@ -1,23 +1,9 @@
 import { type Handlers } from "$fresh/server.ts";
 
-let latest: string | undefined;
-
 export const handler: Handlers = {
-  async GET(req) {
+  GET(req) {
     if (req.headers.get("user-agent")?.startsWith("Deno")) {
-      if (!latest) {
-        const res = await fetch("https://jsr.io/@kitsonk/kview/meta.json");
-        if (res.ok) {
-          const { latest: l } = await res.json();
-          latest = l;
-        } else {
-          return Response.error();
-        }
-      }
-      return Response.redirect(
-        `https://jsr.io/@kitsonk/kview/${latest}/install.ts`,
-        307,
-      );
+      return Response.redirect("https://deno.land/x/kview/install.ts", 307);
     }
     return Response.redirect("/", 301);
   },
